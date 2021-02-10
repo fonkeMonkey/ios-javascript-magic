e384a5665c874872bbc01b5a594587de88249210
Pavol Virdzek <pavol.virdzek@avast.com>
2021-02-10 12:16:22 +0100

    * Formated code in ViewController.

--- iOSJavaScriptMagic/ViewController.swift
+++ iOSJavaScriptMagic/ViewController.swift
@@ -9,27 +9,25 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let webView = WKWebView(frame: .zero)
-
-                view.addSubview(webView)
-
-                let layoutGuide = view.safeAreaLayoutGuide
-
-                webView.translatesAutoresizingMaskIntoConstraints = false
-                webView.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor).isActive = true
-                webView.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor).isActive = true
-                webView.topAnchor.constraint(equalTo: layoutGuide.topAnchor).isActive = true
-                webView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
-
-                if let url = URL(string: "https://www.google.com") {
-                    webView.load(URLRequest(url: url))
-                }
+        
+        view.addSubview(webView)
+        
+        let layoutGuide = view.safeAreaLayoutGuide
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor).isActive = true
+        webView.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor).isActive = true
+        webView.topAnchor.constraint(equalTo: layoutGuide.topAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
+        
+        if let url = URL(string: "https://www.google.com") {
+            webView.load(URLRequest(url: url))
+        }
     }
-
-
 }
 
