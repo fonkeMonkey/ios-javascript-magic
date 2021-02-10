0fe67c47f564141c5e91fa44db90224977168133
Pavol Virdzek <pavol.virdzek@avast.com>
2021-02-10 12:09:58 +0100

    + Added and embedded ViewController with WebView.

---
+++ iOSJavaScriptMagic/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  iOSJavaScriptMagaic
+//
+//  Created by Pavol Virdzek on 09/02/2021.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let webView = WKWebView(frame: .zero)
+
+                view.addSubview(webView)
+
+                let layoutGuide = view.safeAreaLayoutGuide
+
+                webView.translatesAutoresizingMaskIntoConstraints = false
+                webView.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor).isActive = true
+                webView.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor).isActive = true
+                webView.topAnchor.constraint(equalTo: layoutGuide.topAnchor).isActive = true
+                webView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
+
+                if let url = URL(string: "https://www.google.com") {
+                    webView.load(URLRequest(url: url))
+                }
+    }
+
+
+}
+
