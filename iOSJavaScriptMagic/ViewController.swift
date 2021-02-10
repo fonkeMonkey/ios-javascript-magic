973296804c1dd71952e24ff771b7c3cf8b89b1f8
Pavol Virdzek <pavol.virdzek@avast.com>
2021-02-10 12:24:35 +0100

    + Added Custom Web View configurtion which changes google.com's color to Orange.

--- iOSJavaScriptMagic/ViewController.swift
+++ iOSJavaScriptMagic/ViewController.swift
@@ -9,25 +9,37 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let webView = WKWebView(frame: .zero)
-        
+
+        let webView = WKWebView(frame: .zero, configuration: getCustomWebViewConfig())
+
         view.addSubview(webView)
-        
+
         let layoutGuide = view.safeAreaLayoutGuide
-        
+
         webView.translatesAutoresizingMaskIntoConstraints = false
         webView.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor).isActive = true
         webView.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor).isActive = true
         webView.topAnchor.constraint(equalTo: layoutGuide.topAnchor).isActive = true
         webView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
-        
+
         if let url = URL(string: "https://www.google.com") {
             webView.load(URLRequest(url: url))
         }
+    }
+
+    func getCustomWebViewConfig() -> WKWebViewConfiguration{
+        let contentController = WKUserContentController()
+        let scriptSource = "document.body.style.backgroundColor = `orange`;"
+        let script = WKUserScript(source: scriptSource, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        contentController.addUserScript(script)
+
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+
+        return config
     }
 }
 
